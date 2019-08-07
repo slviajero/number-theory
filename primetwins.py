fcecb32d9c9bddb0324820db408dcb7fe1e20c36
Stefan Lenz <stefan.lenz@serverfabrik.de>
2019-08-07 09:48:43 +0200

    Writing a few comments

--- primetwins.py
+++ primetwins.py
@@ -1,16 +1,21 @@
 import math
 
 #
-# Playing around with twins
+# Playing around with twins 
 #
+# the program calculates a few prime numbers and then searches for the twins that are a certain distance appart 
+# (not only twins with a distance of 2)
+#
+# primes are constructed by first creating a list consisting of the first prime which is 2 and then testing numbers 
+
+# we know that the first prime is 2 
 
 primes=[2]
 twins=[]
 
 #
-# get more primes
+# get more primes, we start with 3 and use the fact that we only have to work with prime factors up to sqrt(c)
 #
-
 for c in range(3, 10000):
 	limit=int(math.sqrt(c))
 	for p in primes:
@@ -22,6 +27,9 @@ for c in range(3, 10000):
 
 print("Number of primes:", len(primes))
 
+#
+# get the twins with a distance 2-50
+#
 for o in range(2,50,2):
 	no=0
 	for p in primes:
